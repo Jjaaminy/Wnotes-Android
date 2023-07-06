6509d18a6f1d3dd228ed183f967fb5ba8dda40a1
Jjaaminy <jaaminy18@gmail.com>
2023-07-06 10:39:20 +0200

    edit-save function

--- app/src/main/java/com/example/wnotes/EditActivity.java
+++ app/src/main/java/com/example/wnotes/EditActivity.java
@@ -1,22 +1,71 @@
 package com.example.wnotes;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Set;
+
 
 public class EditActivity extends AppCompatActivity {
+    int idnotes;
     private Button speichern;
     private EditText text;
+    Intent intent = getIntent();
 
+
+    SharedPreferences.Editor editor = getSharedPreferences("edit",MODE_PRIVATE).edit();
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         speichern = findViewById(R.id.button);
-        text = findViewById(R.id.recyclerView);
+        text = findViewById(R.id.edittext);
+
+        idnotes = intent.getIntExtra("id", -1);
+        if (idnotes != -1) {
+            text.setText(MainActivity.notizenliste.get(idnotes));
+        } else {
+            MainActivity.notizenliste.add("");
+            idnotes = MainActivity.notizenliste.size() - 1;
+            MainActivity.adapter.notifyDataSetChanged();
+        }
+
+        text.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence input, int i, int i1, int i2) {
+                MainActivity.notizenliste.set(idnotes, String.valueOf(input));
+                MainActivity.adapter.notifyDataSetChanged();
+                SharedPreferences sharedPreferences = getApplication().getSharedPreferences("noteslist", Context.MODE_PRIVATE);
+                ArrayList<String> set = new ArrayList<String>(MainActivity.notizenliste);
+                sharedPreferences.edit().putStringSet("note", (Set<String>) set).apply();
+
+            }
+
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        }
+        );
     }
 }
--- app/src/main/java/com/example/wnotes/MainActivity.java
+++ app/src/main/java/com/example/wnotes/MainActivity.java
@@ -1,23 +1,79 @@
 package com.example.wnotes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.collection.CircularArray;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button hinzufügen;
-    private ListView list;
 
+    private Button hinzufügen;
+    public ListView list;
+
+    static ArrayList<String> notizenliste = new ArrayList<>();
+    static ArrayAdapter<String> adapter;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.button) {
+
+            // Going from MainActivity to NotesEditorActivity
+            Intent intent = new Intent(getApplicationContext(), EditActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return false;
+    }
+
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         hinzufügen = findViewById(R.id.button);
         list = findViewById(R.id.recyclerView);
+
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("notes", Context.MODE_PRIVATE);
+        ArrayList<String> set = (ArrayList<String>) sharedPreferences.getStringSet("notes", null);
+
+
+        if (set == null) {
+            notizenliste.add("Ex. note");
+        } else {
+            notizenliste = new ArrayList<>(set);
+        }
+
+        adapter = new ArrayAdapter<>(this, R.id.recyclerView, notizenliste);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent = new Intent(getApplicationContext(), EditActivity.class);
+                intent.putExtra("noteid", i);
+                startActivity(intent);
+            }
+        });
     }
 }
